820d7071f43afda54d359fe35a41cbb0f6dff21e
Godwin <dministry356@gmail.com>
2023-08-04 10:52:15 +0100

    added contact page hero image

--- components/ContactPageHeader.tsx
+++ components/ContactPageHeader.tsx
@@ -6,10 +6,25 @@ import Image from 'next/image'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon, ShareIcon } from '@heroicons/react/24/outline'
+import ImgContact from '../public/contactHeroSection.png'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
+
+const links = [
+    { name: 'Open roles', href: '#' },
+    { name: 'Internship program', href: '#' },
+    { name: 'Our values', href: '#' },
+    { name: 'Meet our leadership', href: '#' },
+]
+const stats = [
+    { name: 'Offices worldwide', value: '12' },
+    { name: 'Full-time colleagues', value: '300+' },
+    { name: 'Hours per week', value: '40' },
+    { name: 'Paid time off', value: 'Unlimited' },
+]
+
 
 export default function HomePageHeader() {
     return (
@@ -133,7 +148,50 @@ export default function HomePageHeader() {
                     )}
                 </Disclosure>
                 {/* NavBar End */}
+                <div>
 
+
+                    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+                        <Image
+                            src={ImgContact}
+                            alt="Contact Image Section"
+                            className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+
+                        />
+
+                        <div
+                            className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+                            aria-hidden="true"
+                        >
+                            <div
+                                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                                style={{
+                                    clipPath:
+                                        'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                }}
+                            />
+                        </div>
+                        <div
+                            className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+                            aria-hidden="true"
+                        >
+                            <div
+                                className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+                                style={{
+                                    clipPath:
+                                        'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                                }}
+                            />
+                        </div>
+                        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                            <div className="mx-auto lg:mx-0">
+                                <h2 className="flex items-center justify-center text-4xl font-bold tracking-tight text-white sm:text-6xl text-center">CONTACT US</h2>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
             </main>
 
         </div>
